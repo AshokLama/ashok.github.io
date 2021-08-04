485f7bab871ee8b671663416e88a9a354743c3fd
AshokLama <kosha.amal@gmail.com>
2021-08-04 00:08:15 -0500

    cm
    cm

--- src/js/CleanScreen.js
+++ src/js/CleanScreen.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
     function getRandomColor() {
         var letters = '0123456789ABCDEF';
         var color = '#';
-        for (var i = 0; i < 12; i++) {
+        for (var i = 0; i < 6; i++) {
             color += letters[Math.floor(Math.random() * 16)];
         }
         return color;

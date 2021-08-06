c883683db6d9e5dd6f273d513cb6da7822dee754
AshokLama <kosha.amal@gmail.com>
2021-08-06 11:44:07 -0500

    commit
    commit

---
+++ src/Nodejs/age.js
@@ -0,0 +1,11 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+readline.question('What is your age? ', age => { if (age<16)
+    return console.log(`“You’re not
+allowed to drive in Iowa”`); else
+    return  console.log("“You’re allowed to get a drivers license in\n" +
+        "Iowa”")
+    readline.close();
+});

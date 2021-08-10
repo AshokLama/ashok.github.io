c188a7f5b822610faed36afb281aae0917c73651
AshokLama <kosha.amal@gmail.com>
2021-08-10 02:25:45 -0500

    Cookie and Sessions
    Cookie and Sessions

--- src/W3D5/Q3/index.js
+++ src/W3D5/Q3/index.js
@@ -1,20 +1,41 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-
-app.use(express.urlencoded({extended: false}));
-
-app.use('/css', express.static(path.join(__dirname, 'css'))); //Serving Static Resources
-app.use('/js', express.static(path.join(__dirname, 'js')));
-
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+const path = require('path');
+app.use(express.urlencoded({ extended: false }));
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.get('/', (req, res) => {
+    const date_ob = new Date();
+    const hours = date_ob.getHours();
+    let style = "night.css";
+    if (hours >= 6 && hours < 18) {
+        style = "day.css";
+    }
+    res.send(`
+    <!DOCTYPE html>
+    <html lang='en'>
+        <head>
+            <meta charset="utf-8">
+            <title>Name and age</title>
+            <link rel="stylesheet" href="css/${style}">
+        </head>
+        <body>
+            <form method="post" action="result">
+                Name <input name="name">
+                Age <input name="age">
+                <input type="submit">
+            </form>
+        </body>
+    </html>`);
 });
 app.post('/result', (req, res) => {
-    let {name, age} = req.body;
-
-    res.send(`Welcome ${name}, Age: ${age}`)
+    let name = req.body.name;
+    let age = req.body.age;
+    if (!name) {
+        name = "unknown";
+    }
+    if (!age) {
+        age = "unknown"
+    }
+    res.send(`Welcome ${name}, age ${age}`);
 });
-app.listen(3000, () => {
-    console.log('Your Server is running on 3000');
-});
+app.listen(3000);
